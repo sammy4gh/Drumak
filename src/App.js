56ebd5de218039cf54c3448dcb73bcade898b84d
sammy <aristocrat4gh@gmail.com>
2021-05-28 10:47:00 +0000

    reverted to original version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useSelector} from "react-redux";
+import DrumPads from "./components/DrumPads";
+import Controls from "./components/Controls";
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App" id="drum-machine">
+        <div id={"display"}>
+            <DrumPads/>
+            <Controls/>
+        </div>
+
+    </section>
   );
 }
 
